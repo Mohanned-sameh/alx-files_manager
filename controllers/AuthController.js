5cf0e0ad21f96ece625f31ad002d46299f4a07ae
Mohamed Osama <mossama370@gmail.com>
2024-06-29 16:22:33 +0300

    Done

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,48 +1,20 @@
-import sha1 from 'sha1';
+/* eslint-disable import/no-named-as-default */
 import { v4 as uuidv4 } from 'uuid';
 import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
 
-class AuthController {
-  static async getConnect(request, response) {
-    try {
-      const { email, password } = request.body;
-      if (!email) return response.status(400).json({ error: 'Missing email' });
-      if (!password)
-        return response.status(400).json({ error: 'Missing password' });
+export default class AuthController {
+  static async getConnect(req, res) {
+    const { user } = req;
+    const token = uuidv4();
 
-      const user = await dbClient.getUser({ email });
-      if (!user) return response.status(400).json({ error: 'Unauthorized' });
-
-      if (user.password !== sha1(password))
-        return response.status(400).json({ error: 'Unauthorized' });
-
-      const userId = user.userId;
-      const userToken = uuidv4();
-      await redisClient.set(`auth_${userToken}`, userId, 86400);
-
-      return response.status(200).json({ userToken });
-    } catch (err) {
-      console.log(err);
-      response.status(500).json({ error: 'Server error' });
-    }
+    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
+    res.status(200).json({ token });
   }
 
-  static async getDisconnect(request, response) {
-    try {
-      const userToken = request.header('X-Token');
-      if (!userToken) response.status(401).json({ error: 'Unauthorized' });
+  static async getDisconnect(req, res) {
+    const token = req.headers['x-token'];
 
-      const userId = await redisClient.get(`auth_${userToken}`);
-      if (!userId) response.status(401).json({ error: 'Unauthorized' });
-
-      await redisClient.del(`auth_${userToken}`);
-      response.status(204).end();
-    } catch (err) {
-      console.log(err);
-      response.status(500).json({ error: 'Server error' });
-    }
+    await redisClient.del(`auth_${token}`);
+    res.status(204).send();
   }
 }
-
-export default AuthController;
